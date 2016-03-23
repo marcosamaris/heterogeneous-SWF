c6db30171ae38c7cb1e271f15733823876f204be
marcosamaris <marcos.amaris@gmail.com>
2016-03-23 07:34:03 -0300

    Added new experiments over GPUs with 7 applications of Chameleon

--- swf/csv2swf.sh
+++ swf/csv2swf.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-declare -a apps=(spotrf sgetrf_nopiv sgeqrf)
+declare -a apps=( sgetrf_nopiv sgetrs_nopiv slange sposv spotrf spotri spotrs )
 
 for app in "${apps[@]}"; do
-    mkdir -p CPUs/${app}
+    mkdir -p GPUs/${app}
     for TILE in 64 128 256 320 512 640 768 960 ; do
-        for i in 1 8 20 50 100; do
+        for i in 1 5 10 20 50 100; do
             inputfile=../csv/${app}/${app}-${TILE}-${i}.csv
-            outputfile=./CPUs/${app}/${app}-${TILE}-${i}.swf            
+            outputfile=./GPUs/${app}/${app}-${TILE}-${i}.swf            
             if [ -f $inputfile ];
                 then                
                 grep  M${TILE}x${TILE} $inputfile > Temp
